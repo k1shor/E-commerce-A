632389e549d32171ca8d31dc7281d14754e33c63
k1shor <k1shor@hotmail.com>
2024-11-20 15:49:51 +0545

    Project Initialization

---
+++ e-commerce-backend/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+require('dotenv').config()
+require('./database/connection')
+
+const app = express()
+
+const port = process.env.PORT
+
+
+app.listen(port,  ()=>console.log("APP STARTED SUCCESSFULLY"))
